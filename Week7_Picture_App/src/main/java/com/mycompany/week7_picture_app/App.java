17d90476c7bf941090df979b54768f0b8fe8a020
Miguel Medrano <itlabs@WHB019_12.it.farmingdale.edu>
2021-10-14 14:57:05 -0400

    Second commit

--- Week7_Picture_App/src/main/java/com/mycompany/week7_picture_app/App.java
+++ Week7_Picture_App/src/main/java/com/mycompany/week7_picture_app/App.java
@@ -7,6 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
 /**
  * JavaFX App
@@ -16,11 +20,40 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+    
+     public void start( Stage primaryStage) throws IOException{
+      Image img1 = new Image("https://clipart.world/wp-content/uploads/2021/04/Cartoon-Computer-clipart-transparent-background.png");
+      ImageView imgView1= new ImageView(img1);
+      
+      Image img2 = new Image("https://clipartix.com/wp-content/uploads/2017/07/Computer-clip-art-images-2-clipartset.jpg");
+      ImageView imgView2= new ImageView(img2);
+      
+      StackPane pane = new StackPane(imgView1, imgView2);
+      pane.setStyle("-fx-background-color: black");
+      
+      imgView1.setViewport(new Rectangle2D(200, 500, 300, 300));
+      imgView2.setViewport(new Rectangle2D(200, 500, 300, 300));
+       
+      
+      
+      Scene scene =  new Scene(pane, 700, 550);
+      primaryStage.setTitle("Display Picture");
+      primaryStage.setScene(scene);
+      primaryStage.show();
+      };
+     
+     
+  
+
+    
+    
+    
+    
+   // public void start(Stage stage1) throws IOException {
+       // scene = new Scene(loadFXML("primary"), 640, 480);
+      //  stage1.setScene(scene);
+      //  stage1.show();
+   // }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
--- Week7_Picture_App/src/main/java/com/mycompany/week7_picture_app/PrimaryController.java
+++ Week7_Picture_App/src/main/java/com/mycompany/week7_picture_app/PrimaryController.java
@@ -2,8 +2,26 @@ package com.mycompany.week7_picture_app;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class PrimaryController {
+    
+  // public void start( Stage primaryStage){
+     // Image img = new Image("https://media.gcflearnfree.org/content/55e0730c7dd48174331f5164_01_17_2014/whatisacomputer_pc.jpg");
+     // ImageView imgView= new ImageView(img);
+      
+     // StackPane pane = new StackPane(imgView);
+    ///  pane.setStyle("-fx-background-color: black");
+      
+    //  Scene scene =  new Scene(pane, 500, 350);
+    //  primaryStage.setTitle("Picture");
+    //  primaryStage.setScene(scene);
+     // primaryStage.show(); 
+  // };
 
     @FXML
     private void switchToSecondary() throws IOException {
